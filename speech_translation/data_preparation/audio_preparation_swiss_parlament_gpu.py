7df26cfe87710fa420e8e03e81ba3f720d7220a0
Bogumila Dubel <bogumila.dubel@namics.com>
2021-03-25 08:44:15 +0100

    - commit

--- speech_translation/data_preparation/audio_preparation_swiss_parlament_gpu.py
+++ speech_translation/data_preparation/audio_preparation_swiss_parlament_gpu.py
@@ -5,11 +5,11 @@ from examples.speech_to_text.data_utils import extract_fbank_features, save_df_t
 from pathlib import Path
 import pandas as pd
 
-root_path = "../data/swiss_corpus/"
+root_path = "/cluster/home/dubelbog/data/Swiss_Parliaments_Corpus/"
 path_manifest_swiss = root_path + "test_sample.tsv"
-clip_path = root_path + "clips_sample/"
+clip_path = root_path + "clips/"
 mp3_path = root_path + "mp3/"
-feature_root = Path("../data/sound").absolute() / "fbank"
+feature_root = Path(root_path) / "fbank"
 suffix_flac = ".flac"
 suffix_mp3 = ".mp3"
 ms = 1000
@@ -100,81 +100,5 @@ def preparation():
     if len(train_text) > 0:
         gen_voc(train_text, spm_filename_prefix)
 
-
-# def prepare_audio_files():
-#     stop = 1
-#     counter = 1
-#     manifest = {c: [] for c in MANIFEST_COLUMNS}
-#     translation = open(path_manifest_swiss, "r")
-#     train_text = []
-#     for line, tr in translation:
-#         data = line.split()
-#         track_path = root_path + "segments/" + data[0] + suffix_mp3
-#         if os.path.isfile(track_path):
-#             counter = counter + 1
-#         else:
-#             counter = 1
-#             audio_file = prefix + data[0] + suffix_flac
-#             audio_file = AudioSegment.from_file(audio_file)
-#             audio_file.export(track_path)
-#
-#         track = AudioSegment.from_file(track_path)
-#         process_data(data, counter, track, manifest, tr.rstrip())
-#         train_text.extend(tr.rstrip())
-#         stop = stop + 1
-#         if stop > 10:
-#             df = pd.DataFrame.from_dict(manifest)
-#             split = "dev"
-#             task = "st"
-#             save_df_to_tsv(df, Path("../data/sound") / f"{split}_{task}.tsv")
-#
-#             spm_filename_prefix = f"spm_char_{task}"
-#             # with NamedTemporaryFile(mode="w") as f:
-#             #     for t in train_text:
-#             #         f.write(t + "\n")
-#             #     gen_vocab(
-#             #         Path(f.name),
-#             #         Path("../speech_translation/data/sound") / spm_filename_prefix
-#             #     )
-#             # Generate config YAML
-#             gen_config_yaml(
-#                 Path("../data/sound"),
-#                 spm_filename_prefix + ".model",
-#                 yaml_filename=f"config_{task}.yaml",
-#                 specaugment_policy="lb",
-#                 )
-#             return
-
-
 preparation()
-
-# m4a_audio = \
-#     AudioSegment.from_file("/Users/bdubel/Documents/ZHAW/BA/data/europarl_st/pt_es/clips/en.20080924.31.3-260.m4a")
-# m4a_audio.export("/Users/bdubel/Documents/ZHAW/BA/data/europarl_st/pt_es/segments/test.mp3",
-#                  format="mp3")
-#
-# song = AudioSegment.from_file("/Users/bdubel/Documents/ZHAW/BA/data/europarl_st/pt_es/segments/test.mp3")
-#
-# interval_1 = 8.95 * 1000
-#
-# path = "/Users/bdubel/Documents/ZHAW/BA/data/europarl_st/pt_es/segments/first_track.mp3"
-# first_track = song[:interval_1]
-# first_track.export(path, format="mp3")
-#
-# print("frame_rate", first_track.frame_rate)
-# print("duration_seconds", first_track.duration_seconds)
-# print("sample_width", first_track.sample_width)
-# print("frame_width", first_track.frame_width)
-# print("frame_count", first_track.frame_count())
-# waveform_1, sample_rate_1 = torchaudio.load(path)
-# print(waveform_1)
-# print(sample_rate_1)
-# print(int(1 + ((first_track.duration_seconds*1000) - 25) / 10))
-#
-# first_track = first_track.set_frame_rate(frame_rate=48000)
-# first_track.export("/Users/bdubel/Documents/ZHAW/BA/data/europarl_st/pt_es/segments/first_track_1.mp3",
-#                    format="mp3")
-# print("AFTER")
-# print("frame_rate", first_track.frame_rate)
-# print("duration_seconds", first_track.duration_seconds)
 
